aa983c02db6fd0692979a51748f74a878fb5a07f
Zhiqiang Zhang <zqzhang2101@gmail.com>
2023-12-03 12:47:31 -0500

    Fix bug

--- slack_gpt_bot.py
+++ slack_gpt_bot.py
@@ -1,6 +1,9 @@
+from utils import (N_CHUNKS_TO_CONCAT_BEFORE_UPDATING, OPENAI_API_KEY,
+                   SLACK_APP_TOKEN, SLACK_BOT_TOKEN, WAIT_MESSAGE,
+                   num_tokens_from_messages, process_conversation_history,
+                   update_chat)
 from openai import OpenAI
 
-client = OpenAI(api_key=OPENAI_API_KEY)
 import os
 from dotenv import load_dotenv
 from slack_bolt import App
@@ -8,19 +11,11 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 load_dotenv()
 
-GPT_MODEL="gpt-4-1106-preview"
+GPT_MODEL = "gpt-4-1106-preview"
 
-SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
-from utils import (N_CHUNKS_TO_CONCAT_BEFORE_UPDATING, OPENAI_API_KEY,
-                   SLACK_APP_TOKEN, SLACK_BOT_TOKEN, WAIT_MESSAGE,
-                   num_tokens_from_messages, process_conversation_history,
-                   update_chat)
-
+client = OpenAI(api_key=OPENAI_API_KEY)
 app = App(token=SLACK_BOT_TOKEN)
-
 
 
 def get_conversation_history(channel_id, thread_ts):
@@ -44,15 +39,16 @@ def command_handler(body, context):
         )
         reply_message_ts = slack_resp['message']['ts']
         conversation_history = get_conversation_history(channel_id, thread_ts)
-        messages = process_conversation_history(conversation_history, bot_user_id)
+        messages = process_conversation_history(
+            conversation_history, bot_user_id)
         num_tokens = num_tokens_from_messages(messages)
         print(f"Number of tokens: {num_tokens}")
 
         openai_response = client.chat.completions.create(model=GPT_MODEL,
-        messages=messages,
-        stream=True,
-        temperature=0.2,
-        max_tokens=2000)
+                                                         messages=messages,
+                                                         stream=True,
+                                                         temperature=0.2,
+                                                         max_tokens=2000)
 
         response_text = ""
         ii = 0
@@ -61,7 +57,8 @@ def command_handler(body, context):
                 ii = ii + 1
                 response_text += chunk.choices[0].delta.content
                 if ii > N_CHUNKS_TO_CONCAT_BEFORE_UPDATING:
-                    update_chat(app, channel_id, reply_message_ts, response_text)
+                    update_chat(app, channel_id,
+                                reply_message_ts, response_text)
                     ii = 0
             elif chunk.choices[0].finish_reason == 'stop':
                 update_chat(app, channel_id, reply_message_ts, response_text)
